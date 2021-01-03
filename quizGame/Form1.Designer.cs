7933d114f72496fec638819baf9fa6ffae4faba6
Anhar Ali <67914277+mooict@users.noreply.github.com>
2021-01-03 13:49:22 +0000

    Add files via upload

---
+++ quizGame/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace quizGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.Image = global::quizGame.Properties.Resources.questions;
+            this.pictureBox1.Location = new System.Drawing.Point(22, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(766, 304);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(355, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(433, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 511);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(355, 50);
+            this.button3.TabIndex = 1;
+            this.button3.Tag = "3";
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(433, 511);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(355, 50);
+            this.button4.TabIndex = 1;
+            this.button4.Tag = "4";
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.ClickAnswerEvent);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(7, 333);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(793, 74);
+            this.lblQuestion.TabIndex = 2;
+            this.lblQuestion.Text = "Question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 576);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Simple Quiz Game MOO ICT";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblQuestion;
+    }
+}
+
---
+++ quizGame/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizGame
+{
+    public partial class Form1 : Form
+    {
+
+        // variables list for this quiz game
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 8;
+
+
+
+        }
+
+        private void ClickAnswerEvent(object sender, EventArgs e)
+        {
+
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+
+
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+
+
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                // work out the percentage here
+                percentage = (int)Math.Round((double)(100 * score) / totalQuestions);
+
+
+                MessageBox.Show("Quiz Ended" + Environment.NewLine +
+                                "You have answered " + score + " questions correcly" + Environment.NewLine +
+                                "Your total percentage is " + percentage + " %" + Environment.NewLine +
+                                "Click Ok to play again"
+
+                    );
+
+                score = 0;
+                questionNumber = 0;
+
+                askQuestion(questionNumber);
+            }
+
+            questionNumber++;
+
+            askQuestion(questionNumber);
+
+
+
+        }
+
+        private void askQuestion(int qnum)
+        {
+
+            
+
+            switch(qnum)
+            {
+
+                case 1:
+
+                    pictureBox1.Image = Properties.Resources.questions;
+                    lblQuestion.Text = "What is the colour of the Sky?";
+
+                    button1.Text = "Blue";
+                    button2.Text = "Yellow";
+                    button3.Text = "Purple";
+                    button4.Text = "Red";
+
+                    correctAnswer = 1;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.questions;
+                    lblQuestion.Text = "Whats is the name of the main character from Iron Man?";
+
+                    button1.Text = "Tony Stank";
+                    button2.Text = "Tony Stark";
+                    button3.Text = "Rody";
+                    button4.Text = "Peter Quill";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 3:
+
+                    pictureBox1.Image = Properties.Resources.fortnite;
+
+                    lblQuestion.Text = "Which Game Publisher Made the game above?";
+
+                    button1.Text = "EA";
+                    button2.Text = "Activision";
+                    button3.Text = "Square Enix";
+                    button4.Text = "Epic Games";
+
+                    correctAnswer = 4;
+
+                    break;
+
+                case 4:
+
+                    pictureBox1.Image = Properties.Resources.questions;
+
+                    lblQuestion.Text = "Which is the captial city of England?";
+
+                    button1.Text = "Birmingham";
+                    button2.Text = "London";
+                    button3.Text = "Brighton";
+                    button4.Text = "liverpool";
+
+                    correctAnswer = 2;
+
+                    break;
+
+                case 5:
+
+                    pictureBox1.Image = Properties.Resources.gears_of_war;
+
+                    lblQuestion.Text = "What is the name of this game";
+
+                    button1.Text = "Gears Of War";
+                    button2.Text = "Call of Duty";
+                    button3.Text = "Battlefield";
+                    button4.Text = "Bionic Commando";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 6:
+
+                    pictureBox1.Image = Properties.Resources.halo;
+
+                    lblQuestion.Text = "Whats the main characters name in this game?";
+
+                    button1.Text = "Altair";
+                    button2.Text = "Lara Craft";
+                    button3.Text = "Master Cheif";
+                    button4.Text = "Drake";
+
+                    correctAnswer = 3;
+
+                    break;
+
+                case 7:
+
+                    pictureBox1.Image = Properties.Resources.csgo;
+
+                    lblQuestion.Text = "What is the name of this game";
+
+                    button1.Text = "Counter Strike Go";
+                    button2.Text = "Call of Duty";
+                    button3.Text = "Battlefield";
+                    button4.Text = "Half Life 3";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 8:
+
+                    pictureBox1.Image = Properties.Resources.witcher3;
+
+                    lblQuestion.Text = "Who is Geralt looking for in this game?";
+
+                    button1.Text = "Victoria";
+                    button2.Text = "Donuts";
+                    button3.Text = "Ciri";
+                    button4.Text = "Yennefer";
+
+                    correctAnswer = 3;
+
+                    break;
+
+
+
+
+            }
+
+
+
+
+        }
+
+    }
+}
